a8e1e213be4d254df2933b3dfcf39f2e96ff3250
varshineck <31338348+varshineck@users.noreply.github.com>
2017-12-06 20:55:53 -0800

    Added AJAX post call, linked all searches and filters

--- course_ret.py
+++ course_ret.py
@@ -118,35 +118,158 @@ def clean_json(search_results):
 
 
 if __name__ == "__main__":
-    inverted_index = makeInvertedIndex(title_list)
-    
-    results_layer1 = search_input(inverted_index,'artificial intelligence')
-    #print(len(results_layer1))
-
-    courses_str = "var courses = ["
-    for result in results_layer1:
-        courses_str += "{ Title: '" + result['Title'] + "' , Subject: '" + result['Subject'] + "' },"
-    courses_str = courses_str[:-1] + "]"    
-    #print(list_test)
-
-    #json_str = clean_json(list_test)
-    #print (json_str)
-
-    #courses_json = json.dumps(courses_list)
-    print(courses_str)
-
-    #writing json data to file
-    text_file = open("courses.js", "w")
-    text_file.write(courses_str)
-    text_file.close()
-    #with open('data.js', 'a') as outfile:
-    	#json.dump(json_str, outfile)
-    	#outfile.write(list_test)
+    print("Things are going okay!")
 
     #keys = ['Title', 'Subject']
     #filter_test = {x:results_layer1[x] for x in keys}
     #print(filter_test)
 
+
+# Filter search_results by enrollment
+def filter_result_by_language(search_results, language):
+    filtered_search_results = []
+    for search_result in search_results:
+        if search_result['f:%s' %(language)]:
+            filtered_search_results.append(search_result)
+    return filtered_search_results
+
+
+def filter_result_by_institute(search_results, institute):
+    filtered_search_results = []
+    for search_result in search_results:
+        if search_result['f:Offered by %s' %(institute)]:
+            filtered_search_results.append(search_result)
+    return filtered_search_results
+
+
+def filter_result_by_shortduration(search_results):
+    filtered_search_results = []
+    for search_result in search_results:
+        if search_result['f:ShortDuration']:
+            filtered_search_results.append(search_result)
+    return filtered_search_results
+
+def filter_result_by_highworkload(search_results):
+    filtered_search_results = []
+    for search_result in search_results:
+        if search_result['f:HighWorkload']:
+            filtered_search_results.append(search_result)
+    return filtered_search_results
+
+def filter_result_by_enrollment(search_results):
+    filtered_search_results = []
+    for search_result in search_results:
+        if search_result['f:Enroll']:
+            filtered_search_results.append(search_result)
+    return filtered_search_results
+
+def filter_result_by_paid_certification(search_results):
+    filtered_search_results = []
+    for search_result in search_results:
+        if search_result['f:PaidCertification']:
+            filtered_search_results.append(search_result)
+    return filtered_search_results
+
+def filter_result_by_highrating(search_results):
+    filtered_search_results = []
+    for search_result in search_results:
+        if search_result['f:HighRating']:
+            filtered_search_results.append(search_result)
+    return filtered_search_results
+
+
+def filter_result_by_selfpace(search_results):
+    filtered_search_results = []
+    for search_result in search_results:
+        if search_result['f:Self Paced Courses']:
+            filtered_search_results.append(search_result)
+    return filtered_search_results
+
+
 def combining_results(query):
-	#print(query)
-    pass
+
+    #obtaining search criteria
+    searchText = query['searchText']
+    print(searchText)
+
+    language = query['language']
+    print(language)
+
+    provider = query['provider']
+    print(provider)
+
+    institute = query['institute']
+    print(institute)
+
+    pricing = query['pricing']
+    print(pricing)
+
+    
+
+    #print(query.items())
+    #value = query['checkboxes[]']
+    #print(value)
+
+
+    inverted_index = makeInvertedIndex(title_list)
+    if searchText != "":
+        results_layer1 = search_input(inverted_index,searchText)
+    else:
+        print("Search is empty!")
+
+    print("This is layer one : \n")
+    print (len(results_layer1))
+
+
+    if language !="":
+        results_layer2 = filter_result_by_language(results_layer1,language)
+    else:
+        results_layer2 = results_layer1
+    print (len(results_layer2))
+
+
+    if provider !="":
+        results_layer3 = filter_result_by_institute(results_layer2,provider)
+    else:
+        results_layer3 = results_layer2
+
+
+    #if pricing !="":
+        #results_layer4 = filter_result_by_free_courses(search_results)
+    #else:
+        #results_layer4 = results_layer3
+    #print (len(results_layer3))
+
+    for key,value in query.items():
+        print("kay" + key)
+        print("values" + value)
+
+    search_results = results_layer3
+
+    for key,value in query.items():
+        if(key == "shortduration"):
+            search_results = filter_result_by_shortduration(search_results)
+        if(key == "easy"):
+            search_results = filter_result_by_highworkload(search_results)
+        if(key == "enrollnow"):
+            search_results = filter_result_by_enrollment(search_results)
+        if(key == "paidcert"):
+            search_results = filter_result_by_paid_certification(search_results)
+        if(key == "rating"):
+            search_results = filter_result_by_highrating(search_results)
+        if(key == "pacing"):
+            search_results = filter_result_by_selfpace(search_results)
+
+    print(len(search_results))
+
+    courses_str = "var courses = ["
+    for result in search_results:
+        courses_str += "{ Title: '" + result['Title'] + "' , Subject: '" + result['Subject'] + "' },"
+    courses_str = courses_str[:-1] + "]"    
+   
+    #print(courses_str)
+
+    #writing json data to file
+    text_file = open("courses.js", "w")
+    text_file.write(courses_str)
+    text_file.close()
